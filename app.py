6c9437c562d4288145b2ad2ce4680210a3259409
jb897 <beachbum.bowman@gmail.com>
2020-09-10 11:45:43 -0400

    Use of url_for

--- app.py
+++ app.py
@@ -3,22 +3,18 @@ app = Flask(__name__)
 
 
 def primes(n):
-
     # Algorithm is Sieve of Eratosthenes
     # Taken from https://www.geeksforgeeks.org/sieve-of-eratosthenes/
     prime_nums = []
     prime = [True] * (n+1)
     p = 2
     while (p*p <= n):
-
         # If prime[p] is not changed, then it is a prime
         if (prime[p]):
-
             # Update all multiples of p
             for i in range(p*p, n+1, p):
                 prime[i] = False
         p += 1
-
     # Add all prime numbers to prime_nums list
     for p in range(2, n+1):
         if prime[p]:
