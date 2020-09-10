d6f08c9cba5bd9233c3fccc2c546537a9d0bfd7c
jb897 <beachbum.bowman@gmail.com>
2020-09-09 22:23:09 -0400

    PEP8 updates, reduce complexity of evens and odds

--- app.py
+++ app.py
@@ -1,75 +1,59 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-def primes(n):
-	prime_nums = []
-	## Algorithm is Sieve of Eratosthenes
-	prime = [True] * (n+1) 
-	p = 2
-	while (p * p <= n): 
-	      
-	    # If prime[p] is not changed, then it is a prime 
-	    if (prime[p] == True): 
-	          
-	        # Update all multiples of p 
-	        for i in range(p * p, n+1, p): 
-	            prime[i] = False
-	    p += 1
-	  
-	# Print all prime numbers 
-	for p in range(2, n+1): 
-		if prime[p]: 
-			prime_nums.append(p)
-	return prime_nums
 
-def getOddsOrEvens(n, mod):
-	return [i for i in range(1, n+1) if i % 2 == mod]
+def primes(n):
+
+    # Algorithm is Sieve of Eratosthenes
+    # Taken from https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+    prime_nums = []
+    prime = [True] * (n+1)
+    p = 2
+    while (p*p <= n):
+
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p]):
+
+            # Update all multiples of p
+            for i in range(p*p, n+1, p):
+                prime[i] = False
+        p += 1
+
+    # Add all prime numbers to prime_nums list
+    for p in range(2, n+1):
+        if prime[p]:
+            prime_nums.append(p)
+    return prime_nums
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/<number>')
+
+@app.route('/<int:number>')
 def number(number):
-	try:
-		num = int(number)
-	except:
-		return render_template('error.html')
-
-	nums = [i for i in range(1, num+1)]
-	return render_template('number.html', title="Numbers", nums=nums)
+    nums = [i for i in range(1, number+1)]
+    return render_template('number.html', title="Numbers", nums=nums)
 
 
-@app.route('/<number>/odd')
+@app.route('/<int:number>/odd')
 def odd(number):
-	try:
-		num = int(number)
-	except:
-		return render_template('error.html')
+    nums = [i for i in range(1, number+1, 2)]
+    return render_template('number.html', title="Odds", nums=nums)
 
-	nums = getOddsOrEvens(num, 1)
-	return render_template('number.html', title="Odds", nums=nums)
 
-@app.route('/<number>/even')
+@app.route('/<int:number>/even')
 def even(number):
-	try:
-		num = int(number)
-	except:
-		return render_template('error.html')
+    nums = [i for i in range(0, number+1, 2)]
+    return render_template('number.html', title="Evens", nums=nums)
 
-	nums = getOddsOrEvens(num, 0)
-	return render_template('number.html', title="Evens", nums=nums)
 
-@app.route('/<number>/prime')
+@app.route('/<int:number>/prime')
 def prime(number):
-	try:
-		num = int(number)
-	except:
-		return render_template('error.html')
+    nums = primes(number)
+    return render_template('number.html', title="Primes", nums=nums)
 
-	nums = primes(num)
-	return render_template('number.html', title="Primes", nums=nums)
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
